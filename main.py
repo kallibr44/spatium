f63709a2173fec50215054a64f8c2c73c7878067
kallibr44 <kallibr44@gmail.com>
2018-08-09 00:21:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from x11_hash import *
-import hashlib
-import pyscrypt
-import psutil, os
-import time
-load = [] # список с данными по нагрузке
-hash = [] # список с готовыми хэшами
+import psutil, os, time, pyscrypt, hashlib,subprocess
+load = []
+hash = []
 def scrypt(text):
  hashed = pyscrypt.hash(password = bytes(text, encoding='utf-8'),
                        salt = b'seasalt',
@@ -12,7 +9,7 @@ def scrypt(text):
                        r = 1,
                        p = 1,
                        dkLen = 32)
- return hashed[2:-1] # обрезаем готовый Scrypt хэш для того, чтобы избаиться от 'b', " ' " перед конвертацией в hex 
+ return hashed[2:-1] # обрезаем готовый Scrypt хэш для того, чтобы избаиться от 'b', " ' " перед конвертацией в hex
 
 def main(text):
  global load
@@ -21,10 +18,10 @@ def main(text):
  sha256 = hashlib.sha256(bytes(text, encoding='utf-8')).hexdigest()
  scryp = scrypt(text).hex()
  x11 = getPoWHash(bytes(text, encoding='utf-8'))[2:-1].hex()
- pid = os.getpid() # получаем id процесса программы
- py = psutil.Process(pid) # создаем объект
- cpu = str(py.cpu_percent()) # считываем нагрузку на CPU
- ram = str(float('{:.3f}'.format(py.memory_info()[1]/(10**6)))) # ситчываем RAM нагрузку в байта и конвертируем в Mb (bytes*(10**6))
+ pid = os.getpid() # получаем идентификатор процесса программы
+ py = psutil.Process(pid)# Создаем объект "Процесс"
+ cpu = str(py.cpu_percent())# считываем нагрузку на CPU
+ ram = str(float('{:.3f}'.format(py.memory_info()[1]/(10**6))))# ситчываем RAM нагрузку в байта и конвертируем в Mb (bytes*(10**6))
  print("CPU : " + cpu + "%")
  print("RAM : " + ram + " Mb")
  print("Текст: %s \n Хэш SHA-256: %s" %(text, sha256))
@@ -36,10 +33,11 @@ def main(text):
 
 if __name__=='__main__':
     text = input("Введите текст для хэш: ")
-    start = time.time()  # старт таймера для отслеживания времени отработки программы
+    start = time.time() # старт таймера для отслеживания времени отработки программы
     main(text)
     d=time.time()-start # запись времени отработки
+    cpu_config = subprocess.check_output('lscpu', shell=True).strip().decode().split("\n") # собираем информацию о процессоре
     file = open("log.txt","wt")
-    file.write("Нагрузка CPU: {0} %\n Использовано RAM: {1} Mb\n Текст: {2} \n Хэш SHA-256: {3} \n хэш Scrypt: {4} \n хэш X11: {5}\n Время выполнения: {6} \n".format(load[0],load[1],text,hash[0],hash[1],hash[2],d))
+    file.write("Модель процессора:{7}\n Количество ядер: {8}\n Нагрузка CPU: {0} %\n Использовано RAM: {1} Mb\n Текст: {2} \n Хэш SHA-256: {3} \n хэш Scrypt: {4} \n хэш X11: {5}\n Время выполнения: {6} \n".format(load[0],load[1],text,hash[0],hash[1],hash[2],d,cpu_config[12][11:],cpu_config[3][7:]))
     file.close()
     print("Complete with %s seconds \n" % (d))
